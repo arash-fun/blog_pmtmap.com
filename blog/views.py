00a27f40ed34617477aca563b359029cceb802c0
arash-fun <arash.funlife@gmail.com>
2021-08-19 11:23:58 +0430

    category.html fixed and shows related category

--- blog/views.py
+++ blog/views.py
@@ -56,7 +56,6 @@ def detail_article(request , slug ):
 def category(request,slug ):
     context = {
         
-        # 'category':get_object_or_404(Category , slug = slug , status =True),
         'category':get_object_or_404(Category , slug = slug , status =True),
 
     }
