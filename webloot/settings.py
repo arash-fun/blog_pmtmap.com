749d852c238fcbe6520be19cbe14e67605ca53c3
arash-fun <arash.funlife@gmail.com>
2021-08-21 10:39:11 +0430

    database set

--- webloot/settings.py
+++ webloot/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'webloot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webaloot',
+        'USER': 'arash',
+        'PASSWORD': '1234',
+
     }
 }
+
 
 
 # Password validation
